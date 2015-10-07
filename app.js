f0b2f4369aa85e97f82bf19cc05819dbb3a3c66c
Ehsan Gazar <me@ehsangazar.com>
2015-10-07 07:11:21 +0330

    Fix

--- app.js
+++ app.js
@@ -29,7 +29,8 @@ var app = require('express')(),
   swig = require('swig'),
   people;
 
-var io = require('socket.io')();
+var server = require('http').Server(app);
+var io = require('socket.io')(server);
 io.on('connection', function (socket) {
   socket.emit('news', { hello: 'world' });
   socket.on('my other event', function (data) {
