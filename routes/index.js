94e4db56a1932ff96f3f1247d1e79084274db2b3
Ehsan Gazar <me@ehsangazar.com>
2015-10-08 06:38:01 +0330

    Finished phase1.

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,12 @@
 var express = require('express');
 var router = express.Router();
+var db = require('../modules/db');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-	
-  res.render('index', { title: 'Express' });
+  db.query("SELECT id,message,username FROM messages ORDER BY id DESC LIMIT 10", function(err, rows, fields) {
+      res.render('index', { title: 'Express', data: rows.reverse() });
+  });
 });
 
 module.exports = router;
