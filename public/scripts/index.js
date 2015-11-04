a8dbdf7e3f0683a4d4821c36436314637a78c21a
Ehsan Gazar <me@ehsangazar.com>
2015-11-04 16:48:57 +0330

    change in index

--- public/scripts/index.js
+++ public/scripts/index.js
@@ -1,32 +1,37 @@
-var url = $(location).attr('href');
-var socket = io.connect(url);
-var username = "Default";
-var username_id = Math.floor((Math.random() * 6) + 1);
-$('#form-new-message').submit(function(event){
-	event.preventDefault();
-	socket.emit('send message',{'message': $('#form-message').val(), 'username': username, 'username_id': username_id });
-	$('#form-message').val('');
-});
-$('#select-username').submit(function(event){
-	event.preventDefault();
-	username = $('#select-username-input').val();
-	if (username != ''){
-		$('#start').slideUp();
-		$('#chat-room').slideDown();
-	}
-	emojify.run(document.getElementById('chat'))
-});
+$(document).ready(function(){	
+	var url = $(location).attr('href');
+	var socket = io.connect(url);
+	var username = "Default";
+	var username_id = Math.floor((Math.random() * 6) + 1);
+	$('#form-new-message').submit(function(event){
+		event.preventDefault();
+		socket.emit('send message',{'message': $('#form-message').val(), 'username': username, 'username_id': username_id });
+		$('#form-message').val('');
+	});
+	$('#select-username').submit(function(event){
+		event.preventDefault();
+		username = $('#select-username-input').val();
+		if (username != ''){
+			$('#start').slideUp();
+			$('#chat-room').slideDown();
+		}
+		emojify.run(document.getElementById('chat'))
+		var objDiv = document.getElementById("chat");
+		objDiv.scrollTop = objDiv.scrollHeight;
+	});
 
-socket.on('new message',function(data){
-	var className = "default";
-	if (data.username_id % 6 == 1){className = 'default';};
-	if (data.username_id % 6 == 2){className = 'primary';};
-	if (data.username_id % 6 == 3){className = 'success';};
-	if (data.username_id % 6 == 4){className = 'info';};
-	if (data.username_id % 6 == 5){className = 'warning';};
-	if (data.username_id % 6 == 6){className = 'danger';};
-	$('#chat').append('<li><span class="label label-'+className+'">'+data.username+'</span>: '+data.message+'</li>')
-	var objDiv = document.getElementById("chat");
-	objDiv.scrollTop = objDiv.scrollHeight;
-	emojify.run(document.getElementById('chat'))
+	socket.on('new message',function(data){
+		var className = "default";
+		if (data.username_id % 6 == 1){className = 'default';};
+		if (data.username_id % 6 == 2){className = 'primary';};
+		if (data.username_id % 6 == 3){className = 'success';};
+		if (data.username_id % 6 == 4){className = 'info';};
+		if (data.username_id % 6 == 5){className = 'warning';};
+		if (data.username_id % 6 == 6){className = 'danger';};
+		$('#chat').append('<li><span class="label label-'+className+'">'+data.username+'</span>: '+data.message+'</li>')
+		var objDiv = document.getElementById("chat");
+		objDiv.scrollTop = objDiv.scrollHeight;
+		emojify.run(document.getElementById('chat'))
+	});
+
 });
